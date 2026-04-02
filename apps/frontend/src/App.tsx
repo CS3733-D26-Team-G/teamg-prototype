e7234b10fd29fc7017ea1e5945c3f73ee373013e
Ace0Hearts <ColinpTruong@gmail.com>
2026-04-02 13:10:28 -0400

    Started Hero Section

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,15 +4,16 @@ import EmployeeForm from './components/EmployeeForm'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import HeroSection from './components/HeroSection'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <div className='hero'>
+        <HeroSection></HeroSection>
+      </div>
       <div className='form-container'>
-        <EmployeeForm></EmployeeForm>
       </div>  
     </>
   )
