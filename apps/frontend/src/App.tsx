0c3b54fd45c7b07a20f53c41aa61d40fb4b5c58f
ThomasKGilbert <thomaskentgilbert@icloud.com>
2026-04-02 21:44:37 -0400

    commit App.tsx

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <div>
-        <Navbar />
+        <Navbar/>
         <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/dashboard" element={<Dashboard />} />
