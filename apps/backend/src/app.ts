265be605e3899913f12dd3bd3c92b4de0ea8218f
Myer <19848162+myerDOT@users.noreply.github.com>
2026-04-04 16:23:10 -0400

    add eslint + prettier + husky

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -3,8 +3,11 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import { prisma } from "./lib/prisma.js";
 
-dotenv.config({ path: process.env.DOTENV_FILE ||
-    (process.env.NODE_ENV === "production" ? ".env.production" : ".env") })
+dotenv.config({
+  path:
+    process.env.DOTENV_FILE ||
+    (process.env.NODE_ENV === "production" ? ".env.production" : ".env"),
+});
 
 const app = express();
 
@@ -19,14 +22,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/employee", async (req, res) => {
-  const allEmployee= await prisma.employee.findMany();
+  const allEmployee = await prisma.employee.findMany();
   console.log(allEmployee);
   res.send(allEmployee);
   // res.sendStatus(200)
 });
 
 app.get("/content", async (req, res) => {
-  const allContent= await prisma.content.findMany();
+  const allContent = await prisma.content.findMany();
   console.log(allContent);
   res.send(allContent);
   // res.sendStatus(200)
