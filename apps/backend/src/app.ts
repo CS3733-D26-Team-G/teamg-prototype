6946b2551eb765f56353953da14ebcf96f37fe80
Myer <19848162+myerDOT@users.noreply.github.com>
2026-04-04 20:04:19 -0400

    create routes content/underwriter and content/business-analyst

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import { prisma } from "./lib/prisma.js";
+
+import contentRouter from "./routes/content.ts";
 
 dotenv.config({
   path:
@@ -10,7 +11,6 @@ dotenv.config({
 });
 
 const app = express();
-
 const port = process.env.PORT;
 
 // Middleware
@@ -21,23 +21,9 @@ app.get("/", (req, res) => {
   res.sendStatus(200);
 });
 
-app.get("/employee", async (req, res) => {
-  const allEmployee = await prisma.employee.findMany();
-  console.log(allEmployee);
-  res.send(allEmployee);
-  // res.sendStatus(200)
-});
-
-app.get("/content", async (req, res) => {
-  const allContent = await prisma.content.findMany();
-  console.log(allContent);
-  res.send(allContent);
-  // res.sendStatus(200)
-});
+app.use("/content", contentRouter);
 
 // Start server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
-export default app;
