bbcb88a4b707e22f8ddc962a1184fec19d4b81d5
Myer <19848162+myerDOT@users.noreply.github.com>
2026-04-07 16:26:49 -0400

    correct merge

--- apps/frontend/src/theme.tsx
+++ apps/frontend/src/theme.tsx
@@ -1,6 +1,6 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-const theme = createTheme({
+let theme = createTheme({
   palette: {
     primary: {
       dark: "#1A1E4B",
@@ -18,53 +18,12 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: "Playfair Display",
     h1: {
       fontFamily: "Playfair Display",
-      fontSize: "xx-large",
       fontWeight: "bold",
-    },
-    h2: {
-      fontFamily: "Playfair Display",
-      fontSize: "x-large",
-      fontWeight: "bold",
-    },
-    h3: {
-      fontFamily: "Playfair Display",
-      fontSize: "large",
-      fontWeight: "bold",
-    },
-    h4: {
-      fontFamily: "Playfair Display",
-      fontSize: "medium",
-      fontWeight: "bold",
-    },
-    h5: {
-      fontFamily: "Playfair Display",
-      fontSize: "small",
-      fontWeight: "bold",
-    },
-    body1: {
-      fontFamily: "Playfair Display",
-      fontSize: "medium",
-      fontWeight: "regular",
-    },
-    body2: {
-      fontFamily: "Playfair Display",
-      fontSize: "small",
-      fontWeight: "regular",
     },
   },
   components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        "#root": {
-          background: "linear-gradient(135deg, #9abce0 0%, #ffffff 48%)",
-          minHeight: "100vh",
-          height: "100%",
-        },
-      },
-    },
     MuiLink: {
       styleOverrides: {
         root: {
@@ -77,10 +36,12 @@ const theme = createTheme({
         },
       },
       defaultProps: {
-        underline: "hover",
+        underline: "hover", // Applies 'underline="hover"' to all Links by default
       },
     },
   },
 });
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
