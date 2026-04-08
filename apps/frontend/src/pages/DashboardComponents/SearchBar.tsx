47bfc36e24655e5278153433e71c4ed9e1385192
ThomasKGilbert <thomaskentgilbert@icloud.com>
2026-04-08 03:19:15 -0400

    Fixed conflicts

--- apps/frontend/src/pages/DashboardComponents/SearchBar.tsx
+++ apps/frontend/src/pages/DashboardComponents/SearchBar.tsx
@@ -1,9 +1,5 @@
 import { useState, type ChangeEvent } from "react";
-<<<<<<< HEAD
 import { TextField, IconButton, formGroupClasses, Box } from "@mui/material";
-=======
-import { TextField, IconButton, formGroupClasses } from "@mui/material";
->>>>>>> main
 import SearchIcon from "@mui/icons-material/Search";
 import InputAdornment from "@mui/material/InputAdornment";
 import "./dashboard.css";
@@ -14,46 +10,39 @@ interface SearchBarProps {
 
 const SearchBar = ({ setSearchQuery }: SearchBarProps) => (
   <div className="search-container">
-<<<<<<< HEAD
     <Box>
-=======
-    <form>
->>>>>>> main
-      <TextField
-        id="search-bar"
-        placeholder="search"
-        variant="outlined"
-        size="small"
-        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-          setSearchQuery(e.target.value);
-        }}
-        slotProps={{
-          input: {
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          },
-        }}
-        sx={{
-          "& .MuiOutlinedInput-notchedOutline": {
-            border: "none",
-          },
-          "&:hover .MuiOutlinedInput-notchedOutline": {
-            border: "none",
-          },
-          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-            border: "none",
-          },
-        }}
-      ></TextField>
-<<<<<<< HEAD
+      <form>
+        <TextField
+          id="search-bar"
+          placeholder="search"
+          variant="outlined"
+          size="small"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setSearchQuery(e.target.value);
+          }}
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            },
+          }}
+          sx={{
+            "& .MuiOutlinedInput-notchedOutline": {
+              border: "none",
+            },
+            "&:hover .MuiOutlinedInput-notchedOutline": {
+              border: "none",
+            },
+            "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+              border: "none",
+            },
+          }}
+        ></TextField>
+      </form>
     </Box>
-=======
-    </form>
->>>>>>> main
   </div>
 );
-
 export default SearchBar;
