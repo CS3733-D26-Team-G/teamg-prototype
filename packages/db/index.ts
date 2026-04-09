fce31499205a29d5d825f8c64e612e4f257c1742
Myer <19848162+myerDOT@users.noreply.github.com>
2026-04-09 15:02:30 -0400

    fix prisma references

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,4 +1,4 @@
 // import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
 
-export { PrismaClient, Prisma } from "./generated/prisma/client.ts";
+export { PrismaClient, Prisma } from "./generated/prisma/client.js";
 // export { PrismaClientKnownRequestError } from "./generated/prisma/client";
