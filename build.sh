04323403bc6da05073b6d6d6cf5ac72f5a36a9de
Myer <19848162+myerDOT@users.noreply.github.com>
2026-04-09 02:09:16 -0400

    idk

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ echo "=== DB GENERATED FILES ==="
 find packages/db/generated/prisma -maxdepth 3 -type f | sort
 echo "=== DB INDEX ==="
 cat packages/db/index.ts
+echo "=== ZOD STUFF ==="
+find packages/zod/generated/schemas -maxdepth 3 -type f | sort
 turbo run build
